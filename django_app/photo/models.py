07f3f0db208c8f7e0640313e7cdb380f69740351
Sunghwan Park <shoark7@gmail.com>
2016-10-25 17:28:37 +0900

    Add intermediate model in Photo app

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -11,12 +11,32 @@ class Album(models.Model):
     def __str__(self):
         return "Album title : {}".format(self.title[:10])
 
+
 class Photo(models.Model):
     album = models.ForeignKey(Album)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=30)
     description = models.TextField(blank=True)
     img = models.ImageField(upload_to='photo')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        through='PhotoLike',
+                                        related_name='photo_set_like_users',
+                                        )
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                           through='PhotoDislike',
+                                           related_name='photo_set_dislike_users')
 
     def __str__(self):
         return "Photo title : {}".format(self.title[:10])
+
+class PhotoLike(models.Model):
+
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PhotoDislike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
