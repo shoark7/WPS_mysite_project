064bc572801ff33bef8ecbc865e064d350c47b93
Sunghwan Park <shoark7@gmail.com>
2016-10-25 16:31:54 +0900

    Implement Photo app's models

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+# from django.contrib.auth import get_user_model
+from django.conf import settings
 # Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return "Album title : {}".format(self.title[:10])
+
+class Photo(models.Model):
+    album = models.ForeignKey(Album)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    img = models.ImageField(upload_to='photo')
+
+    def __str__(self):
+        return "Photo title : {}".format(self.title[:10])
